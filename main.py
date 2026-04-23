9489c4042167572b0c4d7142250915d090920f07
Eduardo Gil <eduardomachadogil1@gmail.com>
2026-04-23 10:37:23 -0300

    fix main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app.pipeline_2 import run_overlay_referencia
 
 if __name__ == "__main__":
     run_overlay_referencia(
-        caminho_img="./arduino_uno_r3_smd_teste_falta.jpeg",
-        caminho_csv="./valido/projeto_kicad/output/bom_footprints.csv",
-        caminho_saida="valido/output/overlay_ref2.png",
+        caminho_img="./data/arduino_uno_r3_smd_teste_falta.jpeg",
+        caminho_csv="./projeto_kicad/output/bom_footprints.csv",
+        caminho_saida="./output/overlay_ref2.png",
     )
